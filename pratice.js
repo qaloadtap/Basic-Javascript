1ac22f6ebf7fc2da74e4f367a4062684a1fc44d2
qaloadtap <qa@loadtap.com>
2021-04-07 21:49:10 +0200

    This is last time for the day

--- pratice.js
+++ pratice.js
@@ -1,6 +1,4 @@
 This is practice file
 editing one more line in local
 Adding one more line from github
-updating the date one more time to understand better about cloning from local to git hub 
-trying one more time lets see if it works
-changing one more time 
+checking one more time from local to global
