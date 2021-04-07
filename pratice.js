f4b7fa4020ed2f6d85eb928cf3e921cb03d90515
qaloadtap <qa@loadtap.com>
2021-04-07 22:03:28 +0200

    Updating both the files

--- pratice.js
+++ pratice.js
@@ -1,5 +1,8 @@
+const { workerData } = require("worker_threads")
+
 This is practice file
 editing one more line in local
 Adding one more line from github
 checking one more time from local to global
 this is from github to local need to update this in local also
+wow worked
