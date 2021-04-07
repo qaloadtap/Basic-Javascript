3a1ed221cbe137712630f6a4eccefa141f18eccf
qaloadtap <64185464+qaloadtap@users.noreply.github.com>
2021-04-07 22:00:51 +0200

    Update pratice.js

--- pratice.js
+++ pratice.js
@@ -2,3 +2,4 @@ This is practice file
 editing one more line in local
 Adding one more line from github
 checking one more time from local to global
+this is from github to local need to update this in local also
